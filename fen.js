98f94ec20587f5337104659b22dedef2b87b8e71
jrbarreto <jjromerobarreto@gmail.com>
2022-03-14 17:13:22 -0300

    first improvement modifiedFen

--- fen.js
+++ fen.js
@@ -6,52 +6,27 @@ const fen3 = "2rqk2r/1p3pp1/p2bbn1p/3p4/3Q1N2/P1N1P1P1/1P3P1P/R3KB1R w KQk - 1 1
 //-----Modificar FEN: BASE -----
 const modifiedFen = fen => {
 
-  const structure = fen.split(" ").slice(0, 1).join('');
-  
-  const modifen = [];
-  for(let i=0; i < structure.length; i++) {
-    switch(structure[i]) {
-      case '1':
-      case 'r':
-      case 'n':
-      case 'b':
-      case 'q':
-      case 'k':
-      case 'R':
-      case 'N':
-      case 'B':
-      case 'Q':
-      case 'K':
-        modifen[i] = 'x';
-        break;
-      case '2':
-        modifen[i] = 'xx';
-        break;
-      case '3':
-        modifen[i] = 'xxx';
-        break;
-      case '4':
-        modifen[i] = 'xxxx';
-        break;
-      case '5':
-        modifen[i] = 'xxxxx';
-        break;
-      case '6':
-        modifen[i] = 'xxxxxx';
-        break;
-      case '7':
-        modifen[i] = 'xxxxxxx';
-        break;
-      case '8':
-        modifen[i] = 'xxxxxxxx';
-        break;
-      default:
-        modifen[i] = structure[i];
-    }
-  }
+  const regex1 = /[1rnbqk]/i;
+  // const regex2 = /[2345678]/;
 
-  const resp = modifen.join('');
+  const structure = fen.split(" ").slice(0, 1).join('').split('');
+
+  const li = structure.map(item => {
+    const rta = item
+      .replace(regex1, 'x')
+      .replace('2', 'xx')
+      .replace('3', 'xxx')
+      .replace('4', 'xxxx')
+      .replace('5', 'xxxxx')
+      .replace('6', 'xxxxxx')
+      .replace('7', 'xxxxxxx')
+      .replace('8', 'xxxxxxxx')
+
+    return rta;
   
+  });
+
+  const resp = li.join('');
   return resp;
 };
 
